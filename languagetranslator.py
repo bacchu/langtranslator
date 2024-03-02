1a9b5aacbdb95242f5c1581f6728b8c58ce07b91
Triv Prasad <bacchu@gmail.com>
2024-03-02 10:56:50 +0530

    Create languagetranslator.py

---
+++ languagetranslator.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import time
+import keyboard
+import os
+import psutil
+
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+# Define language options and their corresponding codes
+lang_options = ['English', 'French', 'German', 'Italian', 'Russian', 'Swedish']
+lang_tuple = (("English","en"), ("French","fr"), ("German","de"), ("Italian","it"), ("Russian","ru"), ("Swedish","sv"))
+
+src_lang_text = 'Select source language:'
+dest_lang_text = 'Select target language:'
+
+def languageselect(src_dest):
+    """
+    Function to create a selectbox for language selection.
+
+    Parameters:
+    - src_dest (str): Text indicating whether it's source or destination language selection.
+
+    Returns:
+    - str: Language code selected by the user.
+    """
+    sel_lang_name = st.selectbox(src_dest, lang_options)
+    index = lang_options.index(sel_lang_name) 
+    return lang_tuple[index][1]
+
+def translate(text, source_language, target_language):
+    """
+    Function to translate text from source language to target language using the Transformers pipeline.
+
+    Parameters:
+    - text (str): Input text to be translated.
+    - source_language (str): Source language code.
+    - target_language (str): Target language code.
+
+    Returns:
+    - str: Translated text.
+    """
+    tokenizer = AutoTokenizer.from_pretrained(f"Helsinki-NLP/opus-mt-{source_language}-{target_language}")
+    model = AutoModelForSeq2SeqLM.from_pretrained(f"Helsinki-NLP/opus-mt-{source_language}-{target_language}")
+
+    translator = pipeline("translation", model=model, tokenizer=tokenizer)
+
+    translation = translator(text, source_language, target_language)
+    translated_text = translation[0]['translation_text']
+    return translated_text
+
+def main():
+    """
+    Main function to create the Streamlit app for language translation.
+    """
+    st.header('Manasi Prasad & Advaita Dubey :dog:', divider='rainbow')
+    st.title('Language Translator using AI')
+    source_text = st.text_area("Enter the text to translate:")
+
+    # Select source and target languages
+    source_language = languageselect(src_lang_text)
+    target_language = languageselect(dest_lang_text) 
+
+    if st.button("Translate"):
+        # Translate text and display result
+        translated_text = translate(source_text, source_language, target_language)
+        st.write("Translated Text:", translated_text)
+
+    # Button to shut down the app
+    exit_app = st.button("Shut Down")
+    if exit_app:
+        # Give a bit of delay for user experience
+        time.sleep(5)
+        # Close streamlit browser tab
+        keyboard.press_and_release('ctrl+w')
+        # Terminate streamlit python process
+        pid = os.getpid()
+        p = psutil.Process(pid)
+        p.terminate()
+
+
+if __name__== "__main__":
+    main()
